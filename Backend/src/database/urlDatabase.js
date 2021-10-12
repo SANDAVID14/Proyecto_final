bbfb90fb92e90a75a0bbe049d512a0430c46a2d2
SANDAVID14 <86736611+SANDAVID14@users.noreply.github.com>
2021-10-11 20:27:38 -0500

    Update urlDatabase.js

--- Backend/src/database/urlDatabase.js
+++ Backend/src/database/urlDatabase.js
@@ -1,5 +1,5 @@
 module.exports={
     //contraseña: mzqud7xq7AYvpmG4
     //db: "mongodb+srv://Luisa1:mzqud7xq7AYvpmG4@ejemplo1.eimsm.mongodb.net/calculadorap?retryWrites=true&w=majority"
-    db: "mongodb+srv://sandro:EXCRI0G493a@cluster0.j7yoy.mongodb.net/calculadorapecuaria?retryWrites=true&w=majority"
+    db: "mongodb+srv://sandro:EXCRI0G493a@cluster0.j7yoy.mongodb.net/calculadorap?retryWrites=true&w=majority"
 }
